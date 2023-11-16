78e26cacbe0f723d4fc0edf76121781fd4178269
leoMao01 <maony.leo@gmail.com>
2023-11-16 17:03:53 +0100

    exo maj

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,12 +4,14 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @bookmarks = Bookmark.new
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+    redirect_to list_path(list)
   end
 
   private
 
   def bookmark_params
-	  params.require(:bookmark).permit(:name)
+	  params.require(:bookmark).permit(:comment)
 	end
 end
