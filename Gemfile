632d79db66a28aa8594b1af1f02bea2e384bbdda
leoMao01 <maony.leo@gmail.com>
2023-11-17 16:48:26 +0100

    exo done

--- Gemfile
+++ Gemfile
@@ -11,7 +11,9 @@ gem "sprockets-rails"
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
 
-gem "dotenv-rails"
+
+gem "cloudinary"
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
@@ -54,6 +56,7 @@ gem "sassc-rails"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mswin mswin64 mingw x64_mingw ]
+  gem "dotenv-rails"
 end
 
 group :development do
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,28 @@
 class BookmarksController < ApplicationController
   def new
-    @bookmarks = Bookmark.new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
   end
 
   def create
+    # on va stocker des valeurs dans bookmark
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.save
-    redirect_to list_path(list)
+    # on va récupérer les valeurs de list (name, id, etc...)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    # on va stocker les valeurs de list dans bookmark
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      #si ça sauvegarde pas, message d'erreur
+    render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def bookmark_params
-	  params.require(:bookmark).permit(:comment)
+	  params.require(:bookmark).permit(:comment, :movie_id)
 	end
+
 end
